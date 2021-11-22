c372c64aaa8c2c771f2705c4979830dc8ee8631e
Hugo Vallada <valladahugo@gmail.com>
2021-11-22 08:35:42 -0300

    Implementa o Rest Template

--- authuser/src/main/java/com/ead/authuser/clients/UserClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -1,12 +1,17 @@
 package com.ead.authuser.clients;
 
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
@@ -33,12 +38,16 @@ public class UserClient {
         log.info("Request URL: {}", url);
 
         try {
+            var responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+
+            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
 
         } catch (HttpStatusCodeException ex) {
             log.error("Ending request / courses userId {}", userId);
         }
 
-        return null;
+        return new PageImpl<>(searchResult);
     }
 
 }

dd4fcd78234991d10542824459763912e9dac157
Hugo Vallada <valladahugo@gmail.com>
2021-11-29 07:33:48 -0300

    Adiciona o load balance no rest template

--- authuser/src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
+++ authuser/src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
@@ -1,6 +1,7 @@
 package com.ead.authuser.configs;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +10,6 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) { return builder.build(); }
 }

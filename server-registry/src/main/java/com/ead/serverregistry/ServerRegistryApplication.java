41a5cbeedba58911eb9b26359d37e5fc7577631a
Hugo Vallada <valladahugo@gmail.com>
2021-11-29 06:48:54 -0300

    Adiciona o eureka client nos microsserviços de negócio

--- server-registry/src/main/java/com/ead/serverregistry/ServerRegistryApplication.java
+++ server-registry/src/main/java/com/ead/serverregistry/ServerRegistryApplication.java
@@ -2,8 +2,10 @@ package com.ead.serverregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServerRegistryApplication {
 
 	public static void main(String[] args) {

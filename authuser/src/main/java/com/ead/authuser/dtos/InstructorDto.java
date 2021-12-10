3a869def37d63983b9614c4d20c9a2af67d0ea43
Hugo Vallada <valladahugo@gmail.com>
2021-12-10 07:59:51 -0300

    Corrige o instructor dto

--- authuser/src/main/java/com/ead/authuser/dtos/InstructorDto.java
+++ authuser/src/main/java/com/ead/authuser/dtos/InstructorDto.java
@@ -1,5 +1,7 @@
 package com.ead.authuser.dtos;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 import javax.validation.constraints.NotNull;
@@ -10,4 +12,9 @@ public class InstructorDto {
 
     @NotNull
     UUID userId;
+
+    @JsonCreator
+    public InstructorDto(@JsonProperty("userId") UUID userId) {
+        this.userId = userId;
+    }
 }
